06cb8e3fdbde92f2d4ae30b61134ec38757f601f
Ezequiel Jimenez <ejimenez@byupathway.edu>
2025-11-04 00:38:11 -0300

    w01 activivy home

---
+++ script/index.js
@@ -0,0 +1,18 @@
+
+// JavaScript from getdates.js        
+function setCurrentYear() {
+    const currentYearSpan = document.getElementById("currentyear");
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+}
+
+function setLastModified() {
+    const lastModifiedParagraph = document.getElementById("lastModified");
+    if (lastModifiedParagraph) {
+        lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+    }
+}
+
+setCurrentYear();
+setLastModified();
